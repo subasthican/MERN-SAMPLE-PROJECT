1e574a603079d8329f19380bf7079ec50c329538
SUBASTHICAN <subasthican@SUBASTHICANs-MacBook-Pro.local>
2025-08-09 16:51:10 +0530

    update taskmodel js 1.1

--- todobackend/models/TaskModel.js
+++ todobackend/models/TaskModel.js
@@ -2,5 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const TaskSchema = new Schema({
-    
-})
+    title: {
+        type: String,
+        required: true,
+        
+    },
+    description:{
+        type: String,
+        
+    },
+},{ timeseries: true });
+
+module.exports = mongoose.model('Task', TaskSchema);
