f240419ade42d8b37b0282664543c3ac751c1a54
SUBASTHICAN <subasthican@SUBASTHICANs-MacBook-Pro.local>
2025-08-09 16:32:40 +0530

    Initial commit

---
+++ todobackend/server.js
@@ -0,0 +1,30 @@
+//using express
+const express = require('express');
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+//create an instance of express
+const app = express();
+
+//define a route
+app.get('/',(req,res)=> { 
+    res.send("hello worlddddd")
+});
+
+//middleware
+app.use((req,res,next) =>
+{
+    console.log('path '+req.path + 'method' + req.method);
+    next();
+})
+
+//db connection
+mongoose.connect(process.env.MANGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+    console.log("db connect sucess server is listening to port"+ process.env.PORT);
+}); 
+    }).catch((error) => console.log( error));
+//start the server
+const port = 3000;
+
